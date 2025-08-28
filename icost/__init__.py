1e15beeb77ba90a6e2f6a8c3478efcd7c0f9dcdd
Asif Newaz <asifnewaz@iut-dhaka.edu>
2025-08-28 17:02:31 +0600

    Update __init__.py

--- icost/__init__.py
+++ icost/__init__.py
@@ -7,6 +7,7 @@ Created on Wed Aug 27 18:21:10 2025
 
 from .__version__ import __version__
 from .icost import iCost
-from .cat_min import categorize_minority_class
+from .categorize_minority_v2 import categorize_minority_class
 
 __all__ = ["iCost", "categorize_minority_class", "__version__"]
+
